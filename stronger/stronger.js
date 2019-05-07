40ec1e6d8c304f0e6a67701bc779894db30d5bc8
Alex Somlo <alexdsomlo@gmail.com>
2019-05-06 21:44:28 -0400

    drawing 2 v1 complete

--- stronger/stronger.js
+++ stronger/stronger.js
@@ -1,5 +1,6 @@
 let camera, scene, renderer, geometry, material, mesh;
 const container = document.getElementById('container');
+let color = 0;
 
 function init() {
     scene = new THREE.Scene();
@@ -12,7 +13,7 @@ function init() {
 
     // radius, width segments, height segments of sphere
     geometry = new THREE.SphereGeometry(1000, 100, 100);
-    material = new  THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+    material = new  THREE.MeshBasicMaterial({color: "hsl(" + color + ", 50%, 50%)", wireframe: true});
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
@@ -24,13 +25,19 @@ function init() {
 }
 
 function animate() {
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.005;
-
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    material.color = new THREE.Color("hsl(" + color + ", 50%, 50%)");
+    color++;
     renderer.render(scene, camera);
 
     requestAnimationFrame(animate);
 }
 
+function timing() {
+    setTimeout(animate, 4700);
+}
+
+
 init();
-animate();
+timing();
